857f58be126a93541f70965db90489ac7d4fb8c8
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com77408854+jauntyjocularjay@users.noreply.github.com>
2024-07-17 09:02:47 -0700

    debugging verbs tests

---
+++ test/Verbs.test.mjs
@@ -0,0 +1,19 @@
+// import {
+//     getCounter,
+//     throwsAnError,
+//     contains,
+//     did,
+//     does,
+//     have,
+//     has,
+//     is,
+//     matches,
+//     recognizes
+// } from '../module/verbs/Verbs.cjs'
+// import { expects } from './Jestr.cjs'
+
+
+
+
+
+// describe('', () => {})
